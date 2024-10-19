835f5bd7c5419217069a77947c8a4added51a2f7
Esteban Stafford <esteban.stafford@unican.es>
2024-10-19 17:23:54 +0200

    Cache tables show cache fields and data

--- gui.c
+++ gui.c
@@ -46,37 +46,159 @@ static GtkWidget *create_memory_table(Computer *computer) {
     gtk_column_view_append_column (GTK_COLUMN_VIEW (column_view), column);
     g_object_unref (column);
 
-//    GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
-
-//    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Address", renderer, "text", 0, NULL));
-//    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Content", renderer, "text", 1, NULL));
-
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), column_view);
     return scrolled_window;
+}
+
+static void setup_cache_cb(GtkSignalListItemFactory *factory, GObject *listitem) {
+    GtkWidget *label = gtk_label_new(NULL);
+    gtk_list_item_set_child(GTK_LIST_ITEM(listitem), label);
+}
+
+static void bind_line_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->line);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_set_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->set);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_valid_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->valid);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_dirty_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->dirty);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_accessed_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->times_accessed);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_last_access_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->last_accessed);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_first_access_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->first_accessed);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_tag_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    char *string = g_strdup_printf("%u", item->tag);
+    gtk_label_set_text(GTK_LABEL(label), string);
+}
+
+static void bind_cache_content_cb(GtkSignalListItemFactory *factory, GtkListItem *listitem) {
+    GtkWidget *label = gtk_list_item_get_child(listitem);
+    CacheLine *item = gtk_list_item_get_item(GTK_LIST_ITEM(listitem));
+    gtk_label_set_text(GTK_LABEL(label), item->content_cache);
 }
 
 static GtkWidget *create_cache_table(GListStore *model, const char *title) {
     GtkWidget *scrolled_window = gtk_scrolled_window_new();
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 
-    GtkWidget *tree_view = gtk_tree_view_new();
-    gtk_tree_view_set_model(GTK_TREE_VIEW(tree_view), GTK_TREE_MODEL(model));
+    GtkSingleSelection *selection = gtk_single_selection_new(G_LIST_MODEL(model));
+    GtkWidget *column_view = gtk_column_view_new(GTK_SELECTION_MODEL(selection));
 
-    GtkCellRenderer *renderer = gtk_cell_renderer_text_new();
+    GtkListItemFactory *factory;
 
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Line", renderer, "text", 0, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Set", renderer, "text", 1, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Valid", renderer, "text", 2, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Dirty", renderer, "text", 3, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Accessed", renderer, "text", 4, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Last Access", renderer, "text", 5, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("First Access", renderer, "text", 6, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Tag", renderer, "text", 7, NULL));
-    gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), gtk_tree_view_column_new_with_attributes("Content", renderer, "text", 8, NULL));
+    // Line
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_line_cb), NULL);
+    GtkColumnViewColumn *column = gtk_column_view_column_new("Line", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
 
-    g_object_set(renderer, "foreground", "color_cache", NULL);  // Set color from color_cache field
+    // Set
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_set_cb), NULL);
+    column = gtk_column_view_column_new("Set", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
 
-    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), tree_view);
+    // Valid
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_valid_cb), NULL);
+    column = gtk_column_view_column_new("Valid", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    // Dirty
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_dirty_cb), NULL);
+    column = gtk_column_view_column_new("Dirty", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    // Accessed
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_accessed_cb), NULL);
+    column = gtk_column_view_column_new("Accessed", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    // Last Access
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_last_access_cb), NULL);
+    column = gtk_column_view_column_new("Last Access", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    // First Access
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_first_access_cb), NULL);
+    column = gtk_column_view_column_new("First Access", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    // Tag
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_tag_cb), NULL);
+    column = gtk_column_view_column_new("Tag", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    // Content
+    factory = gtk_signal_list_item_factory_new();
+    g_signal_connect(factory, "setup", G_CALLBACK(setup_cache_cb), NULL);
+    g_signal_connect(factory, "bind", G_CALLBACK(bind_cache_content_cb), NULL);
+    column = gtk_column_view_column_new("Content", factory);
+    gtk_column_view_append_column(GTK_COLUMN_VIEW(column_view), column);
+    g_object_unref(column);
+
+    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), column_view);
     return scrolled_window;
 }
 
@@ -100,7 +222,6 @@ static GtkWidget *create_cache_widget(Cache *cache, int level) {
         return create_cache_table(G_LIST_STORE(cache->model_data), title);
     }
 }
-
 
 static void activate(GtkApplication *app, gpointer user_data) {
     Computer *computer = (Computer *)user_data;
